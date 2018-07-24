3abaf892b8373078291b597ba0473a4f94b5b3aa
Toni Carpio <toni@creosoftwareinc.com>
2018-07-24 15:36:51 +0800

    Google OAuth Setup (Console,Permission Request and Callback Route) git push

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "1081045715499-lovs0a50utc9ieqfjose7l72ni2qstuc.apps.googleusercontent.com",
+  googleClientSecret: "DjMX1oHhlCrsZUy7m-ZQ78qt"
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,50 @@
-const express = require('express');
+//Web Server Imports
+const express = require("express");
+// Passport JS Imports
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+// Key Imports
+const keys = require("./config/keys");
+
+//Initialize Application
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(
+  //Client ID and Client Secret -  Requirements to access API
+  //CallbackURL - handles token and user profile afer authentication
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Access Token - Additional requests in behalf of the user
+      // Email Contact List Access
+      console.log("Access Token", accessToken);
+      //Refresh Token - Automatically updates access token
+      console.log("Refresh Token", refreshToken);
+      //Profile - contains all user identification
+      console.log("Profile", profile);
+    }
+  )
+);
 
-    res.send({ bye: 'aeon' });
-})
+// Forward Authentication Request to Google, User Permission Grant
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// Authentication, Profile Request
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+// app.get("/", (req, res) => {
+//   res.send({ bye: "aeon" });
+// });
 
 //Environment variables
 //PRODUCTION environment - process.env.PORT - sent by Heroku
