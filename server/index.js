2ac968b4cd85cc38b6b7471e975185246d8aacae
Toni Carpio <toni@creosoftwareinc.com>
2018-08-14 09:55:26 +0800

    Added production setup in Heroku

--- server/index.js
+++ server/index.js
@@ -52,6 +52,21 @@ require("./routes/authRoutes")(app);
 // For Billing
 require("./routes/billingRoutes")(app);
 
+// Express Setup - Run during production (Heroku)
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //such as main.js file or main.css file
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Verifies root route
 app.use("/", (req, res) => {
   res.send({ message: "Welcome to Aeon Hub" });
